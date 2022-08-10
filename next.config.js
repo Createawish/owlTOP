4abde5009c57f96a983206598ef15e456fcbfec9
Createawish <bezlyudovalera1996@gmail.com>
2022-08-11 02:55:38 +0300

    fix:next config

--- next.config.js
+++ next.config.js
@@ -2,9 +2,26 @@ module.exports = {
     images: {
         domains: ['courses-top.ru']
     },
-    webpack(config) {
+    webpack(config, options) {
         config.module.rules.push({
             loader: '@svgr/webpack',
+            issuer:/\.[jt]sx?$/,
+            options: {
+                prettier: false,
+                svgo: true,
+                svgoConfig: {
+                    plugins: [{
+                        name: 'preset-default',
+                        params:{
+                            override:{
+                                removeViewBox: false
+                            }
+                        }
+
+                    }],
+                },
+                titleProp: true,
+            },
             test: /\.svg$/,
         });
 
